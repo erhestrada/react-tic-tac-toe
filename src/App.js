63b157ab8d53bcf4d6d7ea87ba6da6ca7033fafd
Erik <180784081+erhetrada@users.noreply.github.com>
2024-09-21 23:11:21 -0400

    finished tic-tac-toe game with history

--- src/App.js
+++ src/App.js
@@ -6,16 +6,38 @@ function Square({value, onSquareClick}) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
+  //const [xIsNext, setXIsNext] = useState(true);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const [history, setHistory] = useState([Array(9).fill(null)]);
-
   //last entry in history e.g. 1 entry in history currentSquares = history[0]
-  const currentSquares = history[history.length-1];
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    //setXIsNext(!xIsNext);
   }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    //setXIsNext(nextMove % 2=== 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <div className="game">
@@ -23,7 +45,7 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>game-info</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
